cfca3d8c3618b228f3330a60bd2ea385a28d5ae6
Uwe Klawitter <uwe_klawitter@web.de>
2012-06-18 09:47:00 +0200

    Modified LdapManager to return the complete ldap entry and not just the first value of the given value

--- Ldap/LdapManager.php
+++ Ldap/LdapManager.php
@@ -92,7 +92,17 @@ class LdapManager implements LdapManagerInterface
         }
 
         foreach ($this->params['attributes'] as $attr) {
-            call_user_func(array($user, $attr['user_method']), $entry[$attr['ldap_attr']][0]);
+            $ldapValue = $entry[$attr['ldap_attr']];
+            $value = null;
+
+            if ($ldapValue['count'] == 1) {
+                $value = $ldapValue[0];
+            }
+            else {
+                $value = array_slice($ldapValue, 1, $ldapValue['count']);
+            }
+
+            call_user_func(array($user, $attr['user_method']), $value);
         }
 
         $user->setDn($entry['dn']);
