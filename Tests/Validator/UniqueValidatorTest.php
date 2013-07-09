55c5cf71a670521f32ce79c7cb224f6c01ed2813
tiger-seo <tiger.seo@gmail.com>
2013-07-09 04:14:04 +0300

    updating test according to new changes regarding Symfony 2.3

--- Tests/Validator/UniqueValidatorTest.php
+++ Tests/Validator/UniqueValidatorTest.php
@@ -19,6 +19,8 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
 {
     /** @var UniqueValidator */
     private $validator;
+    /** @var ExecutionContext */
+    private $context;
     /** @var \FR3D\LdapBundle\Ldap\LdapManagerInterface|\PHPUnit_Framework_MockObject_MockObject */
     private $ldapManagerMock;
     /** @var Unique */
@@ -28,14 +30,14 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $context = $this->getMockBuilder('Symfony\Component\Validator\ExecutionContext')
+        $this->context = $this->getMockBuilder('Symfony\Component\Validator\ExecutionContext')
                 ->disableOriginalConstructor()
                 ->getMock();
 
         $this->ldapManagerMock = $this->getMock('FR3D\LdapBundle\Ldap\LdapManagerInterface');
         $this->constraint = new Unique(array('username'));
         $this->validator = new UniqueValidator($this->ldapManagerMock);
-        $this->validator->initialize($context);
+        $this->validator->initialize($this->context);
 
         $this->user = new TestUser();
     }
@@ -49,7 +51,7 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
 
         $this->validator->validate($this->user, $this->constraint);
 
-        $this->assertEquals(1, $this->validator->getViolations()->count());
+        $this->assertEquals(1, $this->context->getViolations()->count());
     }
 
     public function testNoViolationsOnUniqueUserProperty()
@@ -61,7 +63,7 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
 
         $this->validator->validate($this->user, $this->constraint);
         
-        $this->assertEquals(0, $this->validator->getViolations()->count());
+        $this->assertEquals(0, $this->context->getViolations()->count());
     }
 
     /**
