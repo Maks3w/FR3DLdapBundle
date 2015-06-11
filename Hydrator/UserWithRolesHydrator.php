97ef388aed42939b3f45fad27b14caa46004edc5
Noles <noles@a3k.net>
2015-06-12 00:39:46 +0200

    Update testing

--- Hydrator/UserWithRolesHydrator.php
+++ Hydrator/UserWithRolesHydrator.php
@@ -25,9 +25,9 @@ class UserWithRolesHydrator extends AbstractHydrator
     {
         $user = parent::hydrate($ldapUserAttributes);
 
-        if (isset($this->params['role']['search'])) {
+        if (isset($this->params['role'] )&& isset($this->params['role']['search'])) {
             $this->addRolesFromLdapGroup($user, $ldapUserAttributes, $this->params['role']['search']['groupNameAttribute']);
-        } elseif ($this->params['role']['memberOf']) {
+        } elseif (isset($this->params['role']) && $this->params['role']['memberOf']) {
             $this->addRolesFromMemberOf($user, $ldapUserAttributes, $this->params['role']['memberOf']['dnSuffixFilter']);
         }            
 
@@ -56,7 +56,7 @@ class UserWithRolesHydrator extends AbstractHydrator
      */
     private function addRolesFromMemberOf(UserInterface $user, array $ldapUserAttributes, $dnSuffixFilter)
     {
-        foreach ($ldapUserAttributes['memberof'] as $group) {
+        foreach ($ldapUserAttributes['memberof'] as $group) {         
             if (preg_match("/^cn=(.*),$dnSuffixFilter/", $group, $roleName)) {
                 $user->addRole(sprintf('ROLE_%s', self::strToSymRoleSchema($roleName[1])));
             }
--- Tests/Hydrator/UserWithRolesHydratorTest.php
+++ Tests/Hydrator/UserWithRolesHydratorTest.php
@@ -3,6 +3,7 @@
 namespace FR3D\LdapBundle\Tests\Hydrator;
 
 use FR3D\LdapBundle\Hydrator\UserWithRolesHydrator;
+use PHPUnit_Framework_Assert as Assert;
 
 
 class UserWithHydratorTest extends AbstractHydratorTestCase
@@ -22,6 +23,7 @@ class UserWithHydratorTest extends AbstractHydratorTestCase
     public function testLdapSearch()
     {
         $params = [
+            'user_class' => 'FR3D\LdapBundle\Model\LdapUser',
             'role' => [ 
                 'search' => [
                     'groupNameAttribute' => 'cn'
@@ -31,21 +33,76 @@ class UserWithHydratorTest extends AbstractHydratorTestCase
 
         $hydrator = new UserWithRolesHydrator($params, $this->getDefaultUserConfig());
         
-        $roles = [
-            'count' => 3,
-            0 => 'ROLE1',
-            1 => 'ROLE2',
-            2 => 'ROLE3',
+        $groups = [
+            'count' => 2,
+            0 => [
+                'cn' => [
+                    0 => 'ROLE1'
+                ],
+                'dn' => 'cn=ROLE2, ou=group, dc=host, dc=foo',
+                'member' => [
+                    0 => 'cn=test_username, ou=people, dc=host, dc=foo',
+                ],
+            ],
+            1 => [
+                'cn' => [
+                    0 => 'ROLE2'
+                ],
+                'dn' => 'cn=ROLE2, ou=group, dc=host, dc=foo',
+                'member' => [
+                    0 => 'cn=test_username, ou=people, dc=host, dc=foo'
+                ],
+            ],
         ];
-
+              
         $entry = [
-            'dn' => 'ou=group, dc=host, dc=foo',
-            'groups' => $roles,
+            'dn' => 'cn=test_username, ou=people, dc=host, dc=foo',
+            'groups' => $groups,
+        ];
+        
+        $roles = [
+            0 => 'ROLE_ROLE1',
+            1 => 'ROLE_ROLE2',
         ];
 
-        $user = $this->hydrator->hydrate($entry);
+        $user = $hydrator->hydrate($entry);
 
         $this->assertValidHydrateReturn($user);
-        Assert::assertEquals(array_slice($roles, 1), $user->getRoles());
+        Assert::assertEquals($roles, $user->getRoles());
+    }
+    
+    public function testMemberOf()
+    {
+        $params = [
+            'user_class' => 'FR3D\LdapBundle\Model\LdapUser',
+            'role' => [ 
+                'memberOf' => [
+                    'dnSuffixFilter' => 'ou=group,dc=host,dc=foo'
+                ]
+            ]
+        ];
+
+        $hydrator = new UserWithRolesHydrator($params, $this->getDefaultUserConfig());
+        
+        $memberOf = [
+            'count' => 2,
+            0 => 'cn=ROLE1,ou=group,dc=host,dc=foo',
+            1 => 'cn=ROLE2,ou=group,dc=host,dc=foo',
+        ];
+              
+        $entry = [
+            'dn' => 'cn=test_username, ou=people, dc=host, dc=foo',
+            'memberof' => $memberOf,
+        ];
+        
+        $roles = [
+            0 => 'ROLE_ROLE1',
+            1 => 'ROLE_ROLE2',
+        ];
+
+        $user = $hydrator->hydrate($entry);
+
+        $this->assertValidHydrateReturn($user);
+        Assert::assertEquals($roles, $user->getRoles());
     }
 }
